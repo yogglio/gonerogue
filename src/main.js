a6f8c8d9ba3a72f9c63cf31ef4ecb4dc3fa1e28a
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-11-08 14:32:48 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createClient} from 'contentful'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+
+window.contentfulClient = createClient({
+    accessToken: '76315850d37454a8de92a5affb94cb59beb62982d768811fd0c4a05fc5bee229',
+    space: 'cupfip093pb1'
+});
+
+contentfulClient.getEntries()
+    .then((response) => console.log(response.items))
+    .catch(console.error);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -13,6 +14,21 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+    mounted: function(){
+        const element = document.getElementById("map")
+        const options = {
+            zoom: 14,
+            center: new google.maps.LatLng(47.071467, 8.277621)
+        }
+        this.map = new google.maps.Map(element, options);
+    }
 }
 </script>
+
+<style scoped lang="scss">
+  #map {
+    height: 80vh;
+    width: 80vw;
+  }
+</style>
