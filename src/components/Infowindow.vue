196beb0566cefcb8b7eda870de69a1a315e5c346
Tin Nguyen <trungtinroberto.nguyen@hslu.stud.ch>
2018-11-22 15:12:13 +0100

    new

--- src/components/Infowindow.vue
+++ src/components/Infowindow.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="infowindow">
-        <div>{{place.fields.name}}</div>
-        <div>{{place.fields.description}}</div>
-        <div>{{place.fields.location}}</div>
-        <div>Rogue rating: {{place.fields.rating}}</div>
-        <img height="150px" width="200px" v-bind:src=place.fields.photos[0].fields.file.url />
+         <div class = "name">
+          <h3> {{place.fields.name}}</h3>
+             </div>
+        
+        <img class = "rating" v-bind:src="rating">
+        <div class ="image"> <img v-bind:src=place.fields.photos[0].fields.file.url /> </div>
+
+        <div class ="description">{{place.fields.description}}</div>
         <div v-on:click="$emit('closeInfowindow')">close</div>
     </div>
 </template>
@@ -12,13 +15,29 @@
 <script>
     export default {
         name: 'infowindow',
+        data: function(){
+            return {
+                rating: ""
+                
+                }
+            
+        },
         props: {
             place: Object
         },
         methods: {
         },
         mounted (){
-
+            console.log(this.place.fields.rating)
+            if (this.place.fields.rating===1){
+                this.rating=require("../assets/level1.svg")
+            }
+            if (this.place.fields.rating===2){
+                this.rating=require("../assets/level2.svg")
+            }
+            if (this.place.fields.rating===3){
+                this.rating=require("../assets/level3.svg")
+            }
         }
     }
 </script>
@@ -28,10 +47,41 @@
         position: absolute;
         width: 100%;
         height: 100vh;
-        background-color: rgba(255,255,255,0.7);
+        background-color: rgba(29, 31, 54, 0.8);
         top: 0;
         left: 0;
+        display: grid;
+        grid-template-columns: 100%;
+        grid-template-rows: 10% 20% 40% 20% 10%;
+     .name {
+         color: white;
+         background-color: $background-title;
+         border: 10px solid white;
+         border-radius: 3px;
+}   
+    .image {
+        width: 100%;
+        height: 100%;
+
+        img{
+        width: 90%;
+        height: 90%;
+        }
     }
+    .rating {
+        width: 100%;
+        height: 50%;
+        display: flex;
+        justify-self: center; 
+        align-self: center;
+    }
+   
+
+
+
+    }
+    
+
 
 
 </style>
