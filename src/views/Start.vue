01d2c84ff8d6294eab3463b70801090e89f1b595
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-11-23 10:10:22 +0100

    timer

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -15,7 +15,7 @@
                 text:'',
                 message: "Welcome to the World of Gone Rogue <br>"+
                 "<br>" +
-                "Travel in a new environment by going to places that aren't the 'norm'.<br>" +
+                "Travel in a new environment by going to places that aren't the norm.<br>" +
                 "Select a Rogue Level to take on a challenge based on your preferences."
             }
         },
@@ -48,7 +48,7 @@
                 }
         },
         mounted(){
-            setTimeout(this.showTutorial, 3000)
+            setTimeout(this.showTutorial, 2600)
         }
     }
 </script>
