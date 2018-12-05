7b3d66160259fdf42f8f87fc68f79c92dbf43bc2
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-12-05 18:20:04 +0100

    pwa

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import {createClient} from 'contentful'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
