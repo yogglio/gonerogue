b562602ab8071f093c24b07e163e66c4871c0a64
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-12-06 09:37:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ let serveStatic = require('serve-static');
 let fs = require('fs');
 
 
-/*app.use(function(req,res,next) {
-    if (!req.secure) {
+app.use(function(req,res,next) {
+    if (req.header('x-forwarded-proto') !== 'https') {
         console.log("HTTP call detected, not allowed");
         return res.redirect('https://' + req.hostname + req.path);
     } else {
         console.log("HTTPs call detected, allowed");
         return next();
     }
-});*/
+});
 
 // serve the index.html as starting page
 app.get('/', function (req, res) {
-    if (!req.secure) {
+/*    if (req.header('x-forwarded-proto') !== 'https') {
         console.log("HTTP call detected, not allowed");
-        res.redirect('https://' + req.hostname + req.path);
+            res.redirect('https://' + req.hostname + req.path);
     } else {
         console.log("HTTPs call detected, allowed");
-    }
+    }*/
     res.sendFile(path.join(__dirname, "dist", "index.html"))
 });
 
