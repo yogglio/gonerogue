56d33e2d4a80c8fed9d4bc72542a55f76e74a60b
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2019-01-04 14:05:15 +0100

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -7,7 +7,7 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|svg|mp4)$/,
-    workbox.strategies.staleWhileRevalidate({
+    workbox.strategies.cacheFirst({
         cacheName: 'images',
         plugins: [
             new workbox.expiration.Plugin({
@@ -15,7 +15,14 @@ workbox.routing.registerRoute(
                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
             }),
         ],
-    }),
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'static-resources',
+    })
 );
 
 workbox.routing.registerRoute(
@@ -25,14 +32,41 @@ workbox.routing.registerRoute(
     }),
 );
 
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
 workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
     workbox.strategies.cacheFirst({
-        cacheName: 'googleapis',
+        cacheName: 'google-fonts-webfonts',
         plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
             new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
                 maxEntries: 30,
             }),
         ],
-    }),
+    })
+);
+
+workbox.routing.registerRoute(
+    /.*(?:googleapis)\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'googleapis',
+    })
+);
+
+workbox.routing.registerRoute(
+    /.*(?:gstatic)\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'gstatic',
+    })
 );
