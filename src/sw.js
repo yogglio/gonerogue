090385b4c4f54522bcc54c59bfaeee0ecda2365a
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2019-01-16 23:30:35 +0100

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -70,14 +70,47 @@ workbox.routing.registerRoute(
     })
 );
 
-self.addEventListener("push", (event) => {
+// listen for push events
+self.addEventListener("push", event => {
     const data = event.data.json();
     console.log("Push Recieved...");
-    event.waitUntil(self.registration.showNotification(data.title, {
-        body: 'Push Notification Subscription Management'
-    }));
+
+    event.waitUntil(
+        self.clients.matchAll().then(function(clientList) {
+            let focused = clientList.some(function(client) {
+                return client.focused;
+            });
+            let notificationMessage;
+            if (focused) {
+                notificationMessage = 'You\'re still here, thanks!';
+            } else if (clientList.length > 0) {
+                notificationMessage = 'You haven\'t closed the page, ' +
+                    'click here to focus it!';
+            } else {
+                notificationMessage = 'You have closed the page, ' +
+                    'click here to re-open it!';
+            }
+
+            self.registration.showNotification(data.title, {
+                body: notificationMessage
+            });
+        })
+    )
 });
 
+// focus or reopen window on click
+self.addEventListener('notificationclick', event => {
+    event.waitUntil(
+        self.clients.matchAll().then(function (clientList) {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return self.clients.openWindow('https://gonerogue.herokuapp.com');
+        })
+    )
+});
+
+// resubscribe for push if expired
 self.addEventListener('pushsubscriptionchange', event => {
     console.log('Subscription expired');
     event.waitUntil(
