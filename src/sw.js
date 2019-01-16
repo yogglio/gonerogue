e09896db1c1602056b865e47ca3e0f30396406a3
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2019-01-16 10:05:55 +0100

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -71,24 +71,9 @@ workbox.routing.registerRoute(
 );
 
 self.addEventListener('push', function(e) {
-
-    var options = {
-        body: 'This notification was generated from a push!',
-        icon: require('../assets/logo.svg'),
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '2'
-        },
-        actions: [
-            {action: 'explore', title: 'Explore this new world',
-                icon: 'images/checkmark.png'},
-            {action: 'close', title: 'Close',
-                icon: 'images/xmark.png'},
-        ]
-    };
+    
     e.waitUntil(
-        self.registration.showNotification('Hello world!', options)
+        self.registration.showNotification('Hello world!', 'stuff')
     );
 });
 
